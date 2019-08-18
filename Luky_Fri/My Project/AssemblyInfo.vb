5928cab6a16a58e2aa71baf0742365ee6daf3301
Luky1234567890 <41448721+Luky1234567890@users.noreply.github.com>
2019-08-18 13:46:08 +0200

    1.2-dev1 srccode update part 2
    O_O

--- Luky_Fri/My Project/AssemblyInfo.vb
+++ Luky_Fri/My Project/AssemblyInfo.vb
@@ -1,4 +1,5 @@
-﻿Imports System
+﻿Imports System.Resources
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -32,4 +33,5 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.1.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.1.0.0")>
+<Assembly: NeutralResourcesLanguage("en")>
